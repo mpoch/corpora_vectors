9259b82cfcd5365f216aa9b95ce19b3e185bbee4
Marc Poch <marc.pochriera@upf.edu>
2013-09-19 12:35:25 +0200

    adding script to start FL servers

---
+++ scripts/freeling_servers_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+fl_dir="/home/mpoch/upf_local/tools/freeling/freeling-3.0"
+
+ca_port="40004"
+es_port="50005"
+en_port="60006"
+
+"$fl_dir/bin/analyze" --server --port "$es_port" --flush -f "$fl_dir/share/freeling/config/es.cfg" &
+"$fl_dir/bin/analyze" --server --port "$en_port" --flush -f "$fl_dir/share/freeling/config/en.cfg" &
+"$fl_dir/bin/analyze" --server --port "$ca_port" --flush -f "$fl_dir/share/freeling/config/ca.cfg" &
+
+# cat test.tmp | /home/mpoch/upf_local/tools/freeling/freeling-3.0/bin/analyzer_client 50005
